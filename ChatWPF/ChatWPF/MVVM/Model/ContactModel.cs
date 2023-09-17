c2657918b0035b81fb800834ed4016d20f336811
VMekh8 <mekhvitaliy88@gmail.com>
2023-09-17 18:58:25 +0300

    Add new items
    - Add a template for ViewListItems
    - Add a model for Contacts and Messages

---
+++ ChatWPF/ChatWPF/MVVM/Model/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWPF.MVVM.Model
+{
+    class ContactModel
+    {
+
+        public string Username { get; set; }
+        public string ImageSource { get; set; }
+        public ObserbableCollection<MessageModel> MyProperty { get; set; }
+    }
+}
--- ChatWPF/ChatWPF/MVVM/ViewModel/MainViewModel.cs
+++ ChatWPF/ChatWPF/MVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ChatWPF.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,14 @@ namespace ChatWPF.MVVM.ViewModel
 {
     class MainViewModel
     {
+        public ObservableCollection<MessageModel> Messages{ get; set; }
+        public ObservableCollection<ContactModel> Contacts{ get; set; }
+
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+        }
     }
 }
