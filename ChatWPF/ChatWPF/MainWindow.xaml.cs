1910fd4d1ba236902f5817646d9332a00ba26b8c
VMekh8 <mekhvitaliy88@gmail.com>
2023-09-17 17:20:52 +0300

    Made a buttons in control panel work

--- ChatWPF/ChatWPF/MainWindow.xaml.cs
+++ ChatWPF/ChatWPF/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace ChatWPF
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximized_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
     }
 }
