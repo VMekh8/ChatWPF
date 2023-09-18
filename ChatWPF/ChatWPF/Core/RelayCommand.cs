ef4c90627971a15ccbd9a45dfb08d3da79bc25e3
VMekh8 <mekhvitaliy88@gmail.com>
2023-09-18 18:51:23 +0300

    Started to do a RelayCommand

---
+++ ChatWPF/ChatWPF/Core/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatWPF.Core
+{
+    class RelayCommand : ICommand
+    {
+      
+
+        private Action<object> execute;
+
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value}
+            remove { CommandManager.RequerySuggested -= value}
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
