ba053e40bcc513ade4dc9afb21b39e5e8efc35e5
VMekh8 <mekhvitaliy88@gmail.com>
2023-09-25 17:58:41 +0300

    make a broadcast the connection to everyone

--- ChatWPF/ChatServer/Program.cs
+++ ChatWPF/ChatServer/Program.cs
@@ -11,16 +11,20 @@ namespace ChatServer
     class Program
     {
         static TcpListener _listener;
-
+        static List<Client> _users;
 
         static void Main(string[] args)
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
             _listener.Start();
+           
+            while(true)
+            {
+            var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+            }
 
-            var client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has connected");
-            Console.Read();
         }
     }
 }
